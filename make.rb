403297bf3884ddcc7dd4402dd8fdb3bf30a5d567
zhuangbiaowei <zhuangbiaowei@gmail.com>
2009-06-09 10:01:09 +0800

    for win32

--- make.rb
+++ make.rb
@@ -1,2 +1,9 @@
-`gcc -c temp.c`
-`gcc -o temp temp.o`
+require 'rbconfig'
+HOST_OS=Config::CONFIG['host_os']
+if HOST_OS=='mswin32'
+  `cl -c /DWIN32 temp.c`
+  `cl temp.obj`
+else
+  `gcc -c temp.c`
+  `gcc -o temp temp.o`
+end
--- temp.c
+++ temp.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef WIN32
+#include <io.h>
+#include <process.h>
+#else
 #include <unistd.h>
+#endif
 
 int main(){
   printf("Hello world!\n");
