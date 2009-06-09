9540e6efd0671817ce8011a9d26211dc4e02e197
zhuangbiaowei <zhuangbiaowei@gmail.com>
2009-06-09 10:04:37 +0800

    for win32

--- make.rb
+++ make.rb
@@ -2,7 +2,7 @@ require 'rbconfig'
 HOST_OS=Config::CONFIG['host_os']
 if HOST_OS=='mswin32'
   `cl -c /DWIN32 temp.c`
-  `cl temp.obj`
+  `link -OUT:temp.exe temp.obj`
 else
   `gcc -c temp.c`
   `gcc -o temp temp.o`
