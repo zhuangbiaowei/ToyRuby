ced6298833599a652ab8d0dc9a09c6539d4aa5e0
zhuangbiaowei <zhuangbiaowei@gmail.com>
2009-06-11 14:12:22 +0800

    rewrite rake file and BuilderShell

--- make.rb
+++
@@ -1,9 +0,0 @@
-require 'rbconfig'
-HOST_OS=Config::CONFIG['host_os']
-if HOST_OS=='mswin32'
-  `cl -c /DWIN32 temp.c`
-  `link -OUT:temp.exe temp.obj`
-else
-  `gcc -c temp.c`
-  `gcc -o temp temp.o`
-end
